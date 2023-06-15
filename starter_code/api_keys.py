c564aaebfb1efb0016f0b02bb0914590bc43cf10
trst1690 <128717373+trst1690@users.noreply.github.com>
2023-06-15 12:45:36 -0500

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "9ac3fbf0443c3da3181383857c7771b0"
+weather_api_key = 
 
 # Geoapify API Key
-geoapify_key = "0bd9f540e9124c80b47ea276af58eff2"
+geoapify_key =
